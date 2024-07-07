e08642707a47409e4edda203b6088767731532dd
Mayur Kamble <mayurkamble0250@gmail.com>
2024-07-07 20:46:47 +0530

    create some variables with different data types and print it

---
+++ dataType.java
@@ -0,0 +1,16 @@
+public class dataType {
+    public static void main(String[] args) {
+        // creating variable
+        int a = 10;
+        short b = 99;
+        long c = 1038334;
+        byte d = 1;
+
+        // printing variable
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+        System.out.println("c = "+c);
+        System.out.println("d = "+d);
+
+    }
+}
