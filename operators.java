947591f5c0dc247de401cad5e059693c4181dc30
Mayur Kamble <mayurkamble0250@gmail.com>
2024-07-07 22:39:20 +0530

    using some operations with some data types

---
+++ operators.java
@@ -0,0 +1,21 @@
+public class operators {
+
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 5;
+        int a = x + y;
+        int mu = x * y;
+        int mi = x - y;
+        float di = (float) x / y;
+        int mo = x % y;
+
+        float z = (2*6) + (4*4) + 2;
+
+        System.out.println(a);
+        System.out.println(mu);
+        System.out.println(mi);
+        System.out.println(di);
+        System.out.println(mo);
+        System.out.println(z);
+    }
+}
