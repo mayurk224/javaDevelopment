33fc11f50786c7bde6efba97668be711305e03e8
Mayur Kamble <mayurkamble0250@gmail.com>
2024-07-12 09:54:53 +0530

    for_loop

---
+++ Loops.java
@@ -0,0 +1,13 @@
+public class Loops {
+    public static void main(String[] args) {
+        // using for loop
+
+        //for( <initialization> ; <continuation test> ; <update>){
+        //  the code you need to be repeated
+        // }
+
+        for( int i = 1; i <= 5 ; i++){
+            System.out.println(i+" square "+(i*i));
+        }
+    }
+}
