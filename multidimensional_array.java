6b66941ab4002e253c62faf4d4ee5541a0ea0efe
Mayur Kamble <mayurkamble0250@gmail.com>
2024-07-17 09:16:09 +0530

    multidimensional_array

---
+++ multidimensional_array.java
@@ -0,0 +1,22 @@
+public class multidimensional_array {
+    public static void main(String[] args) {
+        // MultiDimensional Arrays
+        // a multidimensional array is an array of arrays. each element of a multidimensional array is an array itself
+        // ex: int [][]a = new int[3][4] - two-dimensional array
+        // ex int arr[][] = {{2,7,9},{3,6,1},{7,4,2}}
+        // two-dimensional array is like matrix
+        //       j is for column
+        //       0   1   2
+        //i   0  2   7   9 - i is to read row
+        //    1  3   6   1
+        //    2  7   4   2
+
+        // Printing each elements from above ex using for loop:
+        // int arr[][] = {{2,7,9},{3,6,1},{7,4,2}}
+        // for(int i = 0; i < 3; i++){
+        //     for(int j = 0; j < 3; j++){
+        //      sout(arr[i][j]);
+        //      }
+        // }
+    }
+}
