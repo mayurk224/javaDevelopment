b8b2b3fdf7e229ec6a7485b43ba27292db4f62c2
Mayur Kamble <mayurkamble0250@gmail.com>
2024-07-22 20:44:00 +0530

    Methods

---
+++ Methods.java
@@ -0,0 +1,15 @@
+public class Methods {
+    // A method is a block of code which only runs when it is called.
+    // You can pass data, known as parameters, into a method.
+
+    // Methods are used to perform certain actions.
+    // and then are also known as functions.
+
+    public static void FirstMethod(){
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        FirstMethod();
+    }
+}
