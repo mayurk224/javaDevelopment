68dacc3808cf5c3bcdd5942b485047f0c7e56c98
Mayur Kamble <mayurkamble0250@gmail.com>
2024-07-22 19:13:10 +0530

    Class

---
+++ classes.java
@@ -0,0 +1,29 @@
+public class classes {
+    //In Java, a class is a blueprint or template for creating objects. It defines the properties (attributes)
+    // and behaviors (methods) that the objects created from the class will have.
+    // Here’s a breakdown of what a class in Java entails:
+    //
+    //Key Components of a Class
+    //Attributes (Fields): Variables that hold the state of an object.
+    //Methods: Functions that define the behavior of an object.
+    //Constructors: Special methods used to initialize objects.
+
+    // States
+    String name, color;
+    int age;
+    char sex;
+
+    // Behaviours = Methods
+    // Method: is a block of code which only runs when it gets call.
+    public void Eating(){
+        System.out.println("Eating");
+    }
+
+    public void Drinking(){
+        System.out.println("Drinking");
+    }
+
+    public void Running(){
+        System.out.println("Running");
+    }
+}
