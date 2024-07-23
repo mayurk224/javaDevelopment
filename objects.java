11bf83801d7212cca4d9c55b5394396a2e11d630
Mayur Kamble <mayurkamble0250@gmail.com>
2024-07-23 22:07:30 +0530

    objects

---
+++ objects.java
@@ -0,0 +1,46 @@
+public class objects {
+    //In Java, a class is a blueprint or template for creating objects. It defines the properties (attributes)
+    // and behaviors (methods) that the objects created from the class will have.
+    // Here’s a breakdown of what a class in Java entails:
+    //
+    //Key Components of a Class
+    //Attributes (Fields): Variables that hold the state of an object.
+    //Methods: Functions that define the behavior of an object.
+    //Constructors: Special methods used to initialize objects.
+
+    // States
+    String name, color;
+    int age;
+    char sex;
+
+    // Behaviours = Methods
+    // Method: is a block of code which only runs when it gets call.
+    public void Eating(){
+        System.out.println("Eating");
+    }
+
+    public void Drinking(){
+        System.out.println("Drinking");
+    }
+
+    public void Running(){
+        System.out.println("Running");
+    }
+
+    public static void main(String[] args) {
+        classes stu = new classes();
+
+        stu.name = "Mayur";
+        stu.age = 24;
+        stu.sex = 'm';
+        stu.color = "Asian";
+
+        stu.Eating();
+        stu.Drinking();
+        stu.Running();
+
+        System.out.println(stu.name);
+        System.out.println(stu.age);
+        System.out.println(stu.color);
+    }
+}
