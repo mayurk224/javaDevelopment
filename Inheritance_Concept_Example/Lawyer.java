ca535134f119ffa316e4522d8b023d78640d69b9
Mayur Kamble <mayurkamble0250@gmail.com>
2024-07-28 08:56:18 +0530

    a closer look at objects with an example

--- Inheritance_Concept_Example/Lawyer.java
+++ Inheritance_Concept_Example/Lawyer.java
@@ -2,9 +2,14 @@ package Inheritance_Concept_Example;
 
 public class Lawyer extends Employee{
     private char lawyerPosition;
+    private String name;
 
     public char getLawyerPosition() {
         return 'm';
+    }
+
+    public Lawyer(String newName){
+        this.name = newName;
     }
 
     // increase salary by 500 on original salary
@@ -12,5 +17,8 @@ public class Lawyer extends Employee{
     @Override
     public double getSalary() {
         return super.getSalary() + 500;
+    }
+
+    public Lawyer() {
     }
 }
--- Inheritance_Concept_Example/Main.java
+++ Inheritance_Concept_Example/Main.java
@@ -10,5 +10,18 @@ public class Main {
         System.out.println(marketer.getHours());
 
         System.out.println(lawyer.getSalary());
+
+        Lawyer l1 = new Lawyer("john"); // 'Lawyer l1' is reference & 'new Lawyer("john")' is a value of object
+        Lawyer l2 = new Lawyer("john"); // 'Lawyer l2' is reference & 'new Lawyer("john")' is a value of object
+        Lawyer l3 = l2;                          // 'Lawyer l3' is reference & it refers or assign the l2 object value
+
+        // comparing the reference of the object
+        // == checks the reference of an object
+        System.out.println(l3 == l2);
+        System.out.println(l2 == l3); // True because l3 assign same value of an object, of l2
+        System.out.println(l1 == l2); // False because references are pointing to two different values of an object
+
+        // comparing the objects states
+        System.out.println(l3.equals(l2));
     }
 }
