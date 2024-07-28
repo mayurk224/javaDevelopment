b5fee2913c6dee560911d3ca8563a197f13228c3
Mayur Kamble <mayurkamble0250@gmail.com>
2024-07-28 09:43:33 +0530

    polymorphism

--- Inheritance_Concept_Example/Main.java
+++ Inheritance_Concept_Example/Main.java
@@ -4,9 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Employee employee = new Employee();
         Lawyer lawyer = new Lawyer();
+        Secreatary secreatary = new Secreatary();
+        LegalSecreatary legalSecreatary = new LegalSecreatary();
+        Marketer marketer = new Marketer();
+
         System.out.println(lawyer.getHours());
 
-        Marketer marketer = new Marketer();
         System.out.println(marketer.getHours());
 
         System.out.println(lawyer.getSalary());
@@ -15,6 +18,8 @@ public class Main {
         Lawyer l2 = new Lawyer("john"); // 'Lawyer l2' is reference & 'new Lawyer("john")' is a value of object
         Lawyer l3 = l2;                          // 'Lawyer l3' is reference & it refers or assign the l2 object value
 
+
+        // ** object_II **
         // comparing the reference of the object
         // == checks the reference of an object
         System.out.println(l3 == l2);
@@ -23,5 +28,20 @@ public class Main {
 
         // comparing the objects states
         System.out.println(l3.equals(l2));
+
+
+        // ** Polymorphism **
+        Employee[] emp = new Employee[10];
+        emp[0] = lawyer;
+        emp[1] = secreatary;
+        emp[2] = legalSecreatary;
+        emp[3] = marketer;
+
+        Employee e1 = new Lawyer();
+        Employee e2 = new LegalSecreatary();
+        Employee e3 = new Secreatary();
+        Employee e4 = new Marketer();
+
+        System.out.println(e1.getSalary());
     }
 }
